51c47390232952f5a808b4bdb3a9850773e12cb7
Marko Lipka <markolipka@posteo.de>
2020-11-05 23:34:00 +0100

    modify test name

--- tests/testthat/test_conversions.R
+++ tests/testthat/test_conversions.R
@@ -1,8 +1,14 @@
 context("conversion functions")
 
-test_that("make_column_vector() makes a 1D matrix", {
+test_that("make_column_vector() makes a 1D matrix with n rows", {
   vec <- 1:10
   expect_is(make_column_vector(vec), class = "matrix")
   expect_equal(dim(make_column_vector(vec)), c(length(vec), 1))
+})
+
+test_that("make_row_vector() makes a 1D matrix with n colums", {
+  vec <- 1:10
+  expect_is(make_row_vector(vec), class = "matrix")
+  expect_equal(dim(make_row_vector(vec)), c(1, length(vec)))
 })
 
