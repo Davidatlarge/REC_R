29ef2e49bf41e9722331492deeabedaa987492ea
Kaiser <KaiserD@fzg.local>
2022-09-30 20:07:53 +0200

    make rec() function

---
+++ rec.R
@@ -0,0 +1,92 @@
+
+rec <- function(
+  original_data,
+  N_c = 101,               # Number of computational grid points
+  C_water = 25e3,          # Nutrient concentration in water column (only important for irrigation)
+  # parameters for Tikhonov regularization
+  lambda = 1,              # 'smoothing' parameter lambda
+  alpha_min = 8,           # lowest alpha value for Tikhonov regularisation and ratio criterion ( actually log_10(alpha_min) )
+  alpha_max = 15,          # largest alpha value for Tikhonov regularisation and ratio criterion ( actually log_10(alpha_max) )
+  N_alpha = 301,           # Number of ratio criterion evaluations in the alpha interval, to find the minimum
+  # setting the boundary conditions for the nutrient concentration
+  bnd_cond_type_z_min = 1, # type of boundary condition at the top: 1: for concentration / 2: for derivative
+  bnd_cond_C_z_min = 25e3, # value of nutrient concentration or derivative at top
+  bnd_cond_type_z_max = 1, # type of boundary condition at the bottom: 1: for concentration / 2: for derivative
+  bnd_cond_C_z_max = 5e3   # value of nutrient concentration or derivative at bottom
+  
+) {
+  # ===========  load packages and helping functions -------
+  library(matlab)
+  library(pracma)
+  library(scales)
+  sapply(list.files(paste0(getwd(), "/helping_routines"), pattern = ".R$", full.names = TRUE), source)
+  
+  # =========== some pre - operations ======================================
+  z_data  <- original_data$z
+  z_c     <- matlab::linspace(z_data[1], z_data[length(z_data)], N_c)
+  
+  C_c     <- operate_property(original_data, "C", z_c)$f_c
+  phi     <- operate_property(original_data, "phi", z_c)$f_c
+  omega   <- operate_property(original_data, "omega", z_c)$f_c
+  beta    <- operate_property(original_data, "beta", z_c)$f_c
+  D       <- operate_property(original_data, "D", z_c)$f_c
+  D_b     <- operate_property(original_data, "Db", z_c)$f_c
+  D_total <- D + D_b         # total diffusivity
+  
+  # store the boundary conditions into list
+  bnd_cond <- list( "type_z_min" = bnd_cond_type_z_min,
+                    "C_z_min"    = bnd_cond_C_z_min,
+                    "type_z_max" = bnd_cond_type_z_max,
+                    "C_z_max"    = bnd_cond_C_z_max)
+  
+  # for Tickhonov regularization
+  l_0 <- 1
+  l_1 <- lambda
+  l_2 <- lambda
+  alpha_ticho <- matlab::logspace(alpha_min, alpha_max, N_alpha)
+  
+  # =========== Determine Concentration and Rates =============================
+  con_rate <- calculate_con_rates_lin_sys_tichonov_mean_rate_2(C_water,
+                                                               bnd_cond, 
+                                                               alpha_ticho,
+                                                               l_0,
+                                                               l_1,
+                                                               l_2,
+                                                               z_c,
+                                                               C_c,
+                                                               omega,
+                                                               D_total,
+                                                               beta,
+                                                               phi)
+  
+  # ===========  combine results in an object ==================================
+  rec_out <- list(input_data = original_data,
+                  input_pars = list(N_c=N_c, C_water=C_water, 
+                                    lambda=lambda, alpha_min=alpha_min, alpha_max=alpha_max, N_alpha=N_alpha, 
+                                    bnd_cond_type_z_min=bnd_cond_type_z_min, bnd_cond_C_z_min=bnd_cond_C_z_min, 
+                                    bnd_cond_type_z_max=bnd_cond_type_z_max, bnd_cond_C_z_max=bnd_cond_C_z_max), 
+                  output_data = data.frame(z = z_c, 
+                                           rate = con_rate$R_out, 
+                                           conc = con_rate$C_out),
+                  alpha_opt = con_rate$alpha_opt,
+                  alpha = con_rate$alpha_ticho,
+                  Tichonov_criterium = con_rate$quot_crit)
+  
+  return(rec_out)
+}
+
+# remove helper functions from environment
+#rm( list = sub(".*/(.*).R$", "\\1", list.files(paste0(getwd(), "/helping_routines"), pattern = ".R$", full.names = TRUE)) )
+
+## example
+source("import_from_setup.R")
+df <- import_from_setup("test_case_2_data_delta/")
+test <- rec(original_data = df)
+
+source("evaluation_functions/plot_rec.R")
+index=which.min(-test$Tichonov_criterium)
+plot_rec(test, type = "localmin")
+points(test$alpha[index], -test$Tichonov_criterium[index], col = "red")
+# compare index for optimum alpha and index for local (actually abs) min
+which(test$alpha==test$alpha_opt)
+index
