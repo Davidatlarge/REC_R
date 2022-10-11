41fd43f89f7ad3d6e1a3d43423e93e732d833059
Kaiser <KaiserD@fzg.local>
2022-10-11 09:54:25 +0200

    add output of all indexes

--- help_functions/find_local_minimum.R
+++ help_functions/find_local_minimum.R
@@ -1,6 +1,6 @@
 # find local minima to index the location of optimal alpha values
 find_local_minimum <- function(values, 
-                               which.loc.min = "last" # c("first", "lowest", "last")
+                               which.loc.min = "last" # c("first", "lowest", "last", "all")
                                ) {
   # ---- find local minima ---- 
   locmin <- NULL
@@ -14,14 +14,16 @@ find_local_minimum <- function(values,
   
   # ---- define the index for best alpha ---- 
   if(sum(locmin)>0) { # if there is at least one local minimum
-    if(which.loc.min == "first") {
+    if(which.loc.min == "all") {
+      index <- which(locmin)
+    } else if(which.loc.min == "first") {
       index <- which(locmin)[1] # use the first local minimum
     } else if(which.loc.min == "lowest") {
       index <- which(locmin)[which.min(values[which(locmin)])] # use lowest local minimum
     } else if(which.loc.min == "last") {
       index <- which(locmin)[length(which(locmin))] # use the last local minimum
     } else {
-      print("which.loc.min must be one of 'first', 'lowest', or 'last'")
+      print("which.loc.min must be one of 'first', 'lowest', 'last', or 'all'")
     }
   } else { # if there is no local minimum
     cat("No local minimum of the Tikhonov criterion found. Using absolute minimum. \nBut you can change alpha limits to look for a local minimum.\n")
@@ -35,6 +37,7 @@ find_local_minimum <- function(values,
 # set.seed(2022)
 # v <- runif(20)
 # plot(v, type="l")
+# points(find_local_minimum(v, "all"), v[find_local_minimum(v, "all")], pch = 4)
 # points(find_local_minimum(v), v[find_local_minimum(v)], col = "green")
 # points(find_local_minimum(v, "first"), v[find_local_minimum(v, "first")], col = "red")
 # points(find_local_minimum(v, "lowest"), v[find_local_minimum(v, "lowest")], col = "blue")
