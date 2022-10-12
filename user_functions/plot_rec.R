715d5c6b3b40633a202a67c56660942bf23ec9f6
Kaiser <KaiserD@fzg.local>
2022-10-12 16:21:22 +0200

    fix x scale when type = "results"

--- user_functions/plot_rec.R
+++ user_functions/plot_rec.R
@@ -8,35 +8,38 @@ plot_rec <- function(rec_out,
     old <- par()$mfrow
     par(mfrow = c(2,3))
     for(i in 2:7) {
-      plot(rec_out$input_data[,1]~rec_out$input_data[,i], 
+      plot(rec_out$input_data[,1] ~ rec_out$input_data[,i], 
            ylim = rev(range(rec_out$input_data[,1])), ylab = "z", 
            xlab = colnames(rec_out$input_data)[i])
     }
-    par(mfrow=old)
+    par(mfrow = old)
      
   } else if(type == "results") {
     # ---- plot concentrations and rates ----
     old <- par()$mfrow
-    par(mfrow=c(1,2))
-    for(i in 2:3) {
-      plot(rec_out$output_data[,1]~rec_out$output_data[,i], 
-           type = "l", col = ifelse(i == 2, "green", "red"),
-           ylim = rev(range(rec_out$output_data[,1])), ylab = "z", 
-           xlab = colnames(rec_out$output_data)[i])
-      if(i==2) points(rec_out$input_data$C, rec_out$input_data$z)
-    }
-    par(mfrow=old)
+    par(mfrow = c(1,2))
+    plot(rec_out$output_data$z ~ rec_out$output_data$conc, 
+         type = "l", col = "green",
+         ylim = rev(range(rec_out$output_data$z)), ylab = "z", 
+         xlim = range(c(rec_out$output_data$conc, rec_out$input_data$C)),
+         xlab = colnames(rec_out$output_data)[2])
+    points(rec_out$input_data$C, rec_out$input_data$z)
+    plot(rec_out$output_data$z ~ rec_out$output_data$rate, 
+         type = "l", col = "red",
+         ylim = rev(range(rec_out$output_data$z)), ylab = "z", 
+         xlab = colnames(rec_out$output_data)[3])
+    par(mfrow = old)
    
   } else if(type == "localmin") {
     # ---- plot Tikhonov criterion as function of alpha ---- 
     old <- par()$mfrow
     par(mfrow = c(1,1))
     plot(rec_out$alpha, 
-         -rec_out$Tikhonov_criterium, # y is neg because the local min in calculate_con_rates_lin_sys_Tikhonov_mean_rate_2() is found by `which.min(-quot_crit)`
+         -rec_out$Tikhonov_criterium, # y is neg because the local min in calculate_con_rates_lin_sys_Tikhonov_mean_rate_2() is found using `-quot_crit`
          log = "x", type = "l",
          ylab = "T[alpha]", xlab = "alpha")
     points(rec_out$alpha_opt, -rec_out$Tikhonov_criterium[which(rec_out$alpha==rec_out$alpha_opt)], col = "green")
-    par(mfrow=old)
+    par(mfrow = old)
     
   } else {
     stop('type needs to be one of "input", "results" or "localmin"')
