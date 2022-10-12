ee3596dac8d96058cd82e548d31a1a8d45b9df57
Kaiser <KaiserD@fzg.local>
2022-10-12 20:49:54 +0200

    change the way the local min is found. the previous method would potentially index wring values if consecutive equal values were in the vector

--- help_functions/find_local_minimum.R
+++ help_functions/find_local_minimum.R
@@ -1,21 +1,21 @@
 # find local minima to index the location of optimal alpha values
 find_local_minimum <- function(values, 
                                which.loc.min = "last" # c("first", "lowest", "last", "all")
-                               ) {
+) {
   # ---- find local minima ---- 
-  locmin <- NULL
-  for(i in 1:length(values)) {
-    if(i == 1 | i == length(values)) {
-      locmin[i] <- FALSE # even if the extremes are minima, they are not local minima
-    } else { # check if the middle one in a group of 3 values is the lowest, i.e. a local minimum
-      locmin[i] <- values[i] == min(values[(i-1):(i+1)])
-    }
-  }
+  diffs <- c(0, sign(diff(values))) # the sign of the difference shows if values are decreasing or increasing
+  diff.diffs <- diff(diffs)# at a local min decrease changes to increase and the sign of the difference changes from -1 to 1, so the difference of the sign difference is 2
   
-  # ---- define the index for best alpha ---- 
+  locmin <- FALSE
+  for(i in 2:length(diff.diffs)) {
+    locmin[i] <- ifelse(diff.diffs[i]==2, TRUE, FALSE)
+  }
+  locmin[length(values)] <- FALSE
+  
+  # ---- define the output index ---- 
   if(sum(locmin)>0) { # if there is at least one local minimum
-    if(which.loc.min == "all") {
-      index <- which(locmin)
+    if(which.loc.min == "all") { 
+      index <- which(locmin) # use all local minima
     } else if(which.loc.min == "first") {
       index <- which(locmin)[1] # use the first local minimum
     } else if(which.loc.min == "lowest") {
@@ -36,9 +36,10 @@ find_local_minimum <- function(values,
 
 # set.seed(2022)
 # v <- runif(20)
+# # v <- c(10,10,10,10,9,8,7,6,5,4,5,6,6,6,7,8,7,6,5,5,5,5,4,3,2,2,2,3,4,5,6,7,7,7,6,5,4,3,2,1,1,1,1,1,1,1,1)
 # plot(v, type="l")
 # points(find_local_minimum(v, "all"), v[find_local_minimum(v, "all")], pch = 4)
-# points(find_local_minimum(v), v[find_local_minimum(v)], col = "green")
+# points(find_local_minimum(v, "last"), v[find_local_minimum(v, "last")], col = "green")
 # points(find_local_minimum(v, "first"), v[find_local_minimum(v, "first")], col = "red")
 # points(find_local_minimum(v, "lowest"), v[find_local_minimum(v, "lowest")], col = "blue")
 
