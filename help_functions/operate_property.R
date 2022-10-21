ef9f077a87dcf462ed1c5f8c53997f10c07ade99
Kaiser <KaiserD@fzg.local>
2022-10-21 17:47:55 +0200

    replace pracma::interp1() with approx()

--- help_functions/operate_property.R
+++ help_functions/operate_property.R
@@ -15,7 +15,7 @@ operate_property <- function(original_data,
     z_c    <- make_column_vector(z_c)
     
     # do the linear interpolation to the computational grid
-    f_c <- pracma::interp1(z_data,f_data,  xi = z_c, method = "linear")
+    f_c <- approx(x = z_data, y = f_data, xout = z_c, method = "linear")$y
     cat(paste("Data interval for", parameter,"is correct.\n"))
   } else {
     cat("The z-coordinate at the end points of the interval do not match the z-coordinates of the concentration data\n")
